ee2e42bb943ed9dd31446071c468308f8a8f26a9
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-05-14 22:41:15 +0300

    Implement BannerViewModel

---
+++ DawaaV2/HSbanner/ViewModel/BannerViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  BannerViewModel.swift
+//  DawaaV2
+//
+//  Created by Mohammed Hassanien on 14/05/2026.
+//
+
+import Foundation
+import Combine
+
+@MainActor
+class BannerViewModel: ObservableObject {
+    
+    
+    @Published var state: UIStateBanner = .idel
+    
+    private let repository: BannerRepositoryProtocol
+        init(repository: BannerRepositoryProtocol = BannerRepository()) {
+        self.repository = repository
+    }
+    
+    func fetchBanners(ids: [String]) {
+        self.state = .loading
+        repository.getBanner(ids: ids) { [weak self] result in
+            Task { @MainActor in
+                switch result {
+                case .success(let banners):
+                    self?.state = .success(banners)
+                case .failure(let error):
+                    self?.state = .error(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
