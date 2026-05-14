baebec82890e19205fa2d7d67bd9aac923217956
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-05-14 15:25:38 +0300

    Add banner data model

--- DawaaV2/HSbanner/Model/BannerModel.swift
+++ DawaaV2/HSbanner/Model/BannerModel.swift
@@ -7,7 +7,40 @@
 
 import Foundation
 
-struct BannerModel
+struct BannerResponse: Codable
 {
-    
+    let components:[BannerComponentWrapper]
+}
+
+struct BannerComponentWrapper:Codable{
+    let component: BannerComponent?
+    let errors:[String]?
+    let status:String?
+}
+
+struct BannerComponent:Codable
+{
+    let uid: String?
+    let uuid: String?
+    let typeCode: String?
+    let modifiedtime: String?
+    let name: String?
+    let bannerType: String?
+    let media: BannerMedia?
+    let urlLink: String?
+    let container: String?
+    let mobilelLinkUrl: String?
+}
+struct BannerMedia: Codable {
+    let mobile: BannerImage?
+    let widescreen: BannerImage?
+}
+struct BannerImage: Codable {
+    let code: String?
+    let catalogId: String?
+    let mime: String?
+    let altText: String?
+    let description: String?
+    let url: String?
+    let downloadUrl: String?
 }
