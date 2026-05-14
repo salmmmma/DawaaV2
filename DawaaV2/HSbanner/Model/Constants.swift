149499db625ff5edf2ee982a3578b4595ef7c623
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-05-15 00:03:58 +0300

    feat: add Constants file with banner IDs

---
+++ DawaaV2/HSbanner/Model/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  DawaaV2
+//
+//  Created by Mohammed Hassanien on 14/05/2026.
+//
+
+
+import Foundation
+
+enum Constants {
+    enum BannerIDs {
+        static let homePage: [String] = [
+            "cmsitem_00083000",
+            "cmsitem_00083001",
+            "cmsitem_00083002",
+            "cmsitem_00083003",
+            "cmsitem_00083005"
+        ]
+    }
+}
