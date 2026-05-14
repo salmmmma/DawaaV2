2c394d2784082fb081917b19bb9aa4e072913b5e
Salma Hassanein <113758995+salmmmma@users.noreply.github.com>
2026-05-14 14:33:58 +0300

    Add DawaaV2 project

---
+++ DawaaV2/DawaaV2App.swift
@@ -0,0 +1,17 @@
+//
+//  DawaaV2App.swift
+//  DawaaV2
+//
+//  Created by Mohammed Hassanien on 14/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct DawaaV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
